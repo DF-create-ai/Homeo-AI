162902a6d2203369f089aa8c6fc5709a89b17c6d
DF-create-ai <plight139@gmail.com>
2025-11-12 21:24:56 +0500

    app/src/main/java/com/healthcare/automation/BluetoothManager.java

---
+++ app/src/main/java/com/healthcare/automation/BluetoothManager.java
@@ -0,0 +1,11 @@
+package com.healthcare.automation;
+
+public class BluetoothManager {
+    public BluetoothManager() {
+        // Bluetooth setup
+    }
+    
+    public void startDataReception() {
+        // Receive medical data
+    }
+}
